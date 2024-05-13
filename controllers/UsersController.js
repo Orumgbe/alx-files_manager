c2f2bb1854d7197673f2b5eb180769b1b7b49b43
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-13 08:03:47 -0700

    Class for handling routing

---
+++ controllers/UsersController.js
@@ -0,0 +1,33 @@
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+
+// Handle user related requests
+class UserController {
+  // Saves new user to the database
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    const uExist = await dbClient.client.db(dbClient.database).collection('users').findOne({ email: email });
+    if (!email) {
+      res.status(400).send('Missing email');
+    } else if (!password) {
+      res.status(400).send('Missing password');
+    } else if (uExist) {
+      res.status(400).send('Already  exist');
+    } else {
+      try {
+        // Hash the password and store to database
+        const sha1 = crypto.createHash('sha1');
+        sha1.update(password.toString());
+        const hashedPass = sha1.digest('hex');
+        const newUser = {
+          email: email,
+          password: hashedPass,
+        }
+        const result = await dbClient.client.db(dbClient.database).collection('users').insertOne(newUser);
+        res.status(201).send({ id: result.insertedId, email: newUser.email });
+      } catch (error) {
+        res.status(500).send(`Error creating user - ${error}`);
+      }
+    }
+  }
+}
