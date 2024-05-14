470efc3bd6962880781bc46fa354fb40f390c0d0
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-14 01:57:35 -0700

    check that password and email exist before saving

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,28 +6,30 @@ class UserController {
   // Saves new user to the database
   static async postNew(req, res) {
     const { email, password } = req.body;
-    if (!email) {
-      res.status(400).send('Missing email');
-    }
-    if (!password) {
-      res.status(400).send('Missing password');
-    }
-    try {
-      const uExist = await dbClient.client.db(dbClient.database).collection('users').findOne({ email });
-      if (uExist) {
-        res.status(400).send('Already  exist');
-      } else {
-        // Hash the password and store to database
-        const hashedPass = sha1(password);
-        const newUser = {
-          email,
-          password: hashedPass,
-        };
-        const result = await dbClient.client.db(dbClient.database).collection('users').insertOne(newUser);
-        res.status(201).send({ id: result.insertedId, email: newUser.email });
+    if (!email && !password) {
+      res.status(400).send({ error: 'Missing user email and password' });
+    } else if (!email) {
+      res.status(400).send({ error: 'Missing email' });
+    } else if (!password) {
+      res.status(400).send({ error: 'Missing password' });
+    } else {
+      try {
+        const uExist = await dbClient.client.db(dbClient.database).collection('users').findOne({ email });
+        if (uExist) {
+          res.status(400).send({ error: 'Already  exist' });
+        } else {
+          // Hash the password and store to database
+          const hashedPass = sha1(password);
+          const newUser = {
+            email,
+            password: hashedPass,
+          };
+          const result = await dbClient.client.db(dbClient.database).collection('users').insertOne(newUser);
+          res.status(201).send({ id: result.insertedId, email: newUser.email });
+        }
+      } catch (error) {
+        res.status(500).send(`Error creating user - ${error}`);
       }
-    } catch (error) {
-      res.status(500).send(`Error creating user - ${error}`);
     }
   }
 }
